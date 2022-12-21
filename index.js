d6cf0f5848bf37e4d6b2305711b8e027b1d239f6
Kittipohn Klahan <Kittipornklahan001@gmail.com>
2022-12-21 18:29:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/', (req, res) => {
+    console.log('Pickco')
+    res.send('Hello Pickco!')
+})
+
+app.get('/student', (req, res) => {
+    connection.query(
+        'select * from student',
+        function(err, results, fields) {
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+app.get('/login', (req, res) => {
+    connection.query(
+        'select id , telephone from student ',
+        function(err, results, fields) {
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+app.listen(process.env.PORT || 3000)
+    //connection.end()
